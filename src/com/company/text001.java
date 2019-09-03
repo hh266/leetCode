90e91d39e0a2ad703039ba601f4cab6def69af1d
zhang0319 <1297293405@qq.com>
2019-09-03 23:30:28 +0800

    date 20190903 两数之和

---
+++ src/com/company/text001.java
@@ -0,0 +1,30 @@
+/**
+ * leetcode 题库第一题:两数之和
+ * date 2019/09/03
+ * link https://leetcode-cn.com/problems/two-sum/
+ */
+package com.company;
+
+public class text001 {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int[] array = twoSum(nums,18);
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+    }
+
+    /**
+     * 暴力解法
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i< nums.length; i++){
+            for(int j = i+1; j <nums.length; j++){
+                if(nums[i] + nums[j] == target) return new int[] {i,j};
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
