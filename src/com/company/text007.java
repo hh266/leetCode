99b781c5f66bdbe91e173302c7fb2d841ca0b694
zhang0319 <1297293405@qq.com>
2019-09-19 23:26:19 +0800

    date 20190916 最长公共前缀

--- src/com/company/text007.java
+++ src/com/company/text007.java
@@ -6,15 +6,18 @@ import com.company.testClass.ListNode;
  * leetcode 题库第二题:020有效的括号
  * date 2019/09/19
  * link https://leetcode-cn.com/problems/longest-common-prefix/
- * desc
- *
+ * desc:
  * 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
- *
  * 示例：
- *
  * 输入：1->2->4, 1->3->4
  * 输出：1->1->2->3->4->4
-
+ *
+ * ListNode:
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
  */
 public class text007 {
     /**
