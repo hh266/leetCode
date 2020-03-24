d3e29eda13ee2a28eb10aa43fe60c64fe79eb704
zhang0319 <zhang13685512820>
2020-03-24 17:46:58 +0800

    new life

--- src/com/company/test018.java
+++ src/com/company/test018.java
@@ -22,5 +22,6 @@ public class test018 {
                 return right;
             }
         }
+        return  1;
     }
 }
---
+++ src/com/mediumDifficulty/AddTwoNumbers0002.java
@@ -0,0 +1,10 @@
+/**
+ * @author zch
+ * 两数相加
+ * @number 0002
+ * @link https://leetcode-cn.com/problems/add-two-numbers/
+ * @date 2020/3/24 10:46
+ */
+public class AddTwoNumbers0002 {
+    
+}
