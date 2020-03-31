75fc4de8d3ec5324f68ba1fc2410493f23b4ab0f
zhang0319 <zhang13685512820>
2020-03-31 17:55:23 +0800

    两数相加

---
+++ src/com/mediumDifficulty/N0002AddTwoNumbers.java
@@ -0,0 +1,33 @@
+import base.ListNode;
+
+import java.util.List;
+
+/**
+ * @author zch
+ * 两数相加
+ * @number 0002
+ * @link https://leetcode-cn.com/problems/add-two-numbers/
+ * @date 2020/3/24 10:46
+ */
+public class N0002AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode p = l1, q = l2, curr = head;
+        int flag = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = flag + x + y;
+            flag = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (flag > 0) {
+            curr.next = new ListNode(flag);
+        }
+        return head.next;
+    }
+
+}
