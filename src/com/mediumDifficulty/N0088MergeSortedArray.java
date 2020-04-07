6524c4ddcde8c71fbd637e1bd956b8a2a7003756
zhang0319 <zhang13685512820>
2020-04-07 15:12:36 +0800

    合并两个有序数组

--- src/com/mediumDifficulty/N0088MergeSortedArray.java
+++ src/com/mediumDifficulty/N0088MergeSortedArray.java
@@ -10,7 +10,7 @@ public class N0088MergeSortedArray {
         int p1 = m - 1;
         int p2 = n - 1;
         while(p1 > -1 && p2 > -1){
-            nums1[p1 + p2 + 1] = nums1[p1] > nums1[p2] ? nums1[p1--] : nums1[p2--];
+            nums1[p1 + p2 + 1] = nums1[p1] > nums2[p2] ? nums1[p1--] : nums2[p2--];
         }
         System.arraycopy(nums2, 0, nums1, 0, p2 + 1);
     }
