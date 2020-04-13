4d0b634b9b7987bc0456d2de15ba2c13a1da5237
zhang0319 <zhang13685512820>
2020-04-13 09:55:29 +0800

    四数之和II

--- src/com/mediumDifficulty/N0454FourSumII.java
+++ src/com/mediumDifficulty/N0454FourSumII.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 
 /**
  * @author zch
- * @description 四数之和
+ * @description 四数之和II
  * @link https://leetcode-cn.com/problems/4sum/
  * @date 2020/4/10 16:32
  */
@@ -27,6 +27,28 @@ public class N0454FourSumII {
         for(int key1 : map1.keySet()){
             if(map2.containsKey(0-key1)){
                 res += map1.get(key1) * map2.get(0-key1);
+            }
+        }
+
+        return res;
+    }
+
+    public int fourSumCount2(int[] A, int[] B, int[] C, int[] D) {
+        int res = 0;
+        HashMap<Integer,Integer> map1 = new HashMap<>();
+
+        for(int a : A){
+            for(int b : B){
+                map1.put(a+b, map1.getOrDefault(a+b,0)+1);
+            }
+        }
+
+        for(int c : C){
+            for(int d : D){
+                int flag = 0-(c+d);
+                if(map1.containsKey(flag)){
+                    res += map1.get(flag);
+                }
             }
         }
 
